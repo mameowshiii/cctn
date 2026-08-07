affc130f76def9161bea4a7256d6fb825084ac3e
Jennyy204 <jennydian840@gmail.com>
2026-08-07 16:33:26 +0800

    Restore admin/staff login link on client login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -170,6 +170,10 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/><circle cx="8.5" cy="7" r="4"/><line x1="20" y1="8" x2="20" y2="14"/><line x1="23" y1="11" x2="17" y2="11"/></svg>
                     Create an Account
                 </a>
+
+                <div class="auth-help" style="margin-top: 1.5rem; text-align: center;">
+                    Are you staff or admin? <a href="{{ route('admin.login') }}" style="color: var(--primary); font-weight: 700; text-decoration: none;">Admin Login Here</a>
+                </div>
             </form>
         </div>
         
